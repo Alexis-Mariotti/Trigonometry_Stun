93fb3993dce884c7af581113ad0bd01cba4bb759
enzo.legros <zo.legros@gmail.com>
2025-04-28 17:20:37 +0200

    Level selection menu added

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -3,13 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
-    {
-        SceneManager.LoadScene(1);
-    }
-
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void GoHome()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
