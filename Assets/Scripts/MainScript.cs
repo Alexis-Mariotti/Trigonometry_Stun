5e0bd23393c574cc0086a143294cf448ad7c1479
enzo.legros <zo.legros@gmail.com>
2025-04-29 17:42:45 +0200

    Options activated with esc

---
+++ Assets/Scripts/MainScript.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainScript : MonoBehaviour
+{
+    public GameObject optionsMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (optionsMenu.activeSelf)
+            {
+                optionsMenu.SetActive(false);
+            }
+            else
+            {
+                optionsMenu.SetActive(true);
+            }
+        }
+    }
+     
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void GoHome()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
